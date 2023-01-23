1f222db588da99879a836b2f9bbb189413a6b65a
PiotrZPL <zpl20052020@protonmail.ch>
2023-01-23 12:32:05 +0100

    Fixed errors in formatseconds

--- src/app/utilities/formatseconds.ts
+++ src/app/utilities/formatseconds.ts
@@ -1,14 +1,16 @@
 export function formatseconds(input: number): string {
     let stringinput: string = input.toString();
     let decimalpart: string = stringinput.split(".")[1];
-    if (decimalpart.length === 0) {
-      stringinput += "000";
-    }
-    else if (decimalpart.length === 1) {
-      stringinput += "00";
-    }
-    else if (decimalpart.length === 2) {
-      stringinput += "0";
+    if (decimalpart != undefined) {
+      if (decimalpart.length === 0) {
+        stringinput += "000";
+      }
+      else if (decimalpart.length === 1) {
+        stringinput += "00";
+      }
+      else if (decimalpart.length === 2) {
+        stringinput += "0";
+      }
     }
     return stringinput;
 }
