18fa373304df982680a13c88e24739ea3a3bb529
PiotrZPL <zpl20052020@protonmail.ch>
2023-03-28 14:18:27 +0200

    Fixed formatting of the number of seconds

--- src/app/utilities/formatseconds.ts
+++ src/app/utilities/formatseconds.ts
@@ -2,15 +2,15 @@ export function formatseconds(input: number): string {
     let stringinput: string = input.toString();
     let decimalpart: string = stringinput.split(".")[1];
     if (decimalpart != undefined) {
-      if (decimalpart.length === 0) {
-        stringinput += "000";
-      }
-      else if (decimalpart.length === 1) {
+      if (decimalpart.length === 1) {
         stringinput += "00";
       }
       else if (decimalpart.length === 2) {
         stringinput += "0";
       }
+    }
+    else {
+      stringinput += ".000";
     }
     return stringinput;
 }
